8999a1d1de520750e2606a71851a5cf3f0e4bded
David Salas <88846701+david568303@users.noreply.github.com>
2025-05-07 18:34:45 +0200

    Update anykernel.sh
    Enable device check to avoid flash on non compatible devices

--- AnyKernel3-master/anykernel.sh
+++ AnyKernel3-master/anykernel.sh
@@ -5,12 +5,12 @@
 # begin properties
 properties() { '
 kernel.string=Shas Dream Project
-do.devicecheck=0
+do.devicecheck=1
 do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=
+device.name1=merlin
 supported.versions=
 supported.patchlevels=
 '; } # end properties
